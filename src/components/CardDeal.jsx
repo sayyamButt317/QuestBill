16d88cc2aa22f717ac609b70ecee9c5f2b62fe86
Sayyam Butt <sayyambutt317@gmail.com>
2023-12-26 00:12:48 +0500

    updated

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,25 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Enjoy the convenience of making payments directly through our platform. QuestBills supports a variety of payment methods for your convenience.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
